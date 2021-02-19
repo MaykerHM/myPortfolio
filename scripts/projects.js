9c2cbba47f251b5b04dabf8732d3e815a2c72399
MaykerHM <hydeki8@gmail.com>
2021-02-19 19:45:33 -0300

    doismedicamentos

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,5 +1,12 @@
 export const projects = [
   {
+    title: 'Relatório: dois medicamentos',
+    description:
+      'A aplicação web filtra e extrai os dados do relatório de saídas de medicamentos para usuários que contém mais de 1600 páginas. Gerando uma tabela discriminada por UBS, quantidade de usuários totais por UBS, quantidade de usuários do medicamento 1, 2 e ambos, e a somatória das quantidades. Com estes dados foi possível planejar o quantitativo a ser distribuído por unidade e facilitar a obtenção destes dados para gerar relatórios mensais.',
+    url: 'http://doismedicamentos.mayker.com.br',
+    img: './assets/doismedicamentos.png',
+  },
+  {
     title: 'Porcentagem Limite de Validade',
     description:
       'Este projeto foi desenvolvido com Flutter e está disponível para Android na Google Play Store - "Porcentagem Limite de Validade". Este aplicativo tem o objetivo de auxiliar no cálculo e controle das validades mínimas aceitas de produtos perecíveis, em relação as suas validades totais.',
