eeee471d53d00e59e0f46c128a89f53be106ae77
MaykerHM <hydeki8@gmail.com>
2021-12-12 16:02:13 -0300

    Atualização

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,5 +1,25 @@
 export const projects = [
   {
+    title: 'API NodeJS Typescript, desenvolvida com TDD',
+    description: 'Projeto de API NodeJS com Typescript utilizando TDD.',
+    url: 'https://github.com/MaykerHM/node-typescript-api',
+    img: './assets/github.jpg',
+  },
+  {
+    title: 'NLW #04 - Projeto: Moveit (Milha Extra: Dark Mode)',
+    description:
+      'Projeto desenvolvido no evento Next Level Week 4ª edição na trilha React JS - NLW 04. Após ter acompanhado e concluído a aplicação proposta pelo evento, adicionei a funcionalidade de "Dark mode" para levar a aplicação para o próximo nível.',
+    url: 'https://github.com/MaykerHM/NLW4-Moveit-Next-JS',
+    img: './assets/nlw4-next.png',
+  },
+  {
+    title: 'Conversor de Códigos',
+    description:
+      'São quatro funcionalidades relacionadas a códigos de um sistema do meu trabalho: formatar, extrair, filtrar e ordenar. Sendo o objetivo principal conseguir interligar e manipular as informações sobre os produtos do sistema e da planilha de controle, através dos códigos que é o ponto de ligação entre eles. Através do conversor e dos códigos dos produtos é possível interligar as informações de: quantidade em estoque, consumo médio, previsão de entrega e se há entregas previstas de certo produto ou se será necessário realizar novos faturamentos dos mesmos.',
+    url: 'http://conversor.mayker.com.br/',
+    img: './assets/conversor.png',
+  },
+  {
     title: 'Relatório: dois medicamentos',
     description:
       'A aplicação web filtra e extrai os dados do relatório de saídas de medicamentos para usuários que contém mais de 1600 páginas. Gerando uma tabela discriminada por UBS, quantidade de usuários totais por UBS, quantidade de usuários do medicamento 1, 2 e ambos, e a somatória das quantidades. Com estes dados foi possível planejar o quantitativo a ser distribuído por unidade e facilitar a obtenção destes dados para gerar relatórios mensais.',
@@ -10,8 +30,7 @@ export const projects = [
     title: 'Porcentagem Limite de Validade',
     description:
       'Este projeto foi desenvolvido com Flutter e está disponível para Android na Google Play Store - "Porcentagem Limite de Validade". Este aplicativo tem o objetivo de auxiliar no cálculo e controle das validades mínimas aceitas de produtos perecíveis, em relação as suas validades totais.',
-    url:
-      'https://play.google.com/store/apps/details?id=com.limite_validade_porcentagem.mayker',
+    url: 'https://play.google.com/store/apps/details?id=com.limite_validade_porcentagem.mayker',
     img: './assets/percentageLimit.png',
   },
   {
